46cfec801496a92a0693088359c8d56591c914c8
Otavio Sonda Alenski <otavio.alenski@tivix.com>
2021-01-20 09:33:24 +0000

    Updates

---
+++ precache-manifest.26dbcbf3614843217857e29e48bf08b8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f5b0c8f9e7baad88fa31e5a5266a7a",
+    "url": "whysims.github.io/weather-reactjs/index.html"
+  },
+  {
+    "revision": "e6cef4cf9370841987ab",
+    "url": "whysims.github.io/weather-reactjs/static/css/2.1b67b0c4.chunk.css"
+  },
+  {
+    "revision": "56bb743ac0cbc2f4361e",
+    "url": "whysims.github.io/weather-reactjs/static/css/main.147e2a44.chunk.css"
+  },
+  {
+    "revision": "e6cef4cf9370841987ab",
+    "url": "whysims.github.io/weather-reactjs/static/js/2.dd887a34.chunk.js"
+  },
+  {
+    "revision": "56bb743ac0cbc2f4361e",
+    "url": "whysims.github.io/weather-reactjs/static/js/main.74f551cf.chunk.js"
+  },
+  {
+    "revision": "cf64c64684ab028c111c",
+    "url": "whysims.github.io/weather-reactjs/static/js/runtime-main.b3a8b4d8.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "whysims.github.io/weather-reactjs/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "whysims.github.io/weather-reactjs/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "whysims.github.io/weather-reactjs/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "whysims.github.io/weather-reactjs/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "whysims.github.io/weather-reactjs/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.94bd90455cf66754fd3cc4c618b80a74.js"
+  "whysims.github.io/weather-reactjs/precache-manifest.26dbcbf3614843217857e29e48bf08b8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("whysims.github.io/weather-reactjs/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
